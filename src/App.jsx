9bdf3f46767c32e474ddbb1a2fb55edca5edabcf
Marcelo Briones <ing@marcelobriones.com.ar>
2022-03-23 02:41:13 -0300

    Add prop-types

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,29 @@ import Coin from './components/Coin/Coin';
 import logo from './logo.svg';
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} alt="React logo" className="App-logo" />
-                <h1 className="App-title">Moralis React Dex</h1>
-            </header>
-            <table className="coin-table">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Ticker</th>
-                        <th>Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <Coin name="Bitcoin" ticker="BTC" price="40000 USD" />
-                    <Coin name="Ethereum" ticker="ETH" price="3000 USD" />
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="React logo" className="App-logo" />
+        <h1 className="App-title">Moralis React Dex</h1>
+      </header>
+      <table className="coin-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Ticker</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Coin name="Bitcoin" ticker="BTC" price={9999.99} />
+          <Coin name="Ethereum" ticker="ETH" price={299.99} />
+          <Coin name="Tether" ticker="USDT" price={1.0} />
+          <Coin name="Ripple" ticker="XRP" price={0.2} />
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Coin.css';
+import PropTypes from 'prop-types';
 
 const Coin = ({ name, ticker, price }) => (
-    <tr className="coin-row">
-        <td>{name}</td>
-        <td>{ticker}</td>
-        <td>{price}</td>
-    </tr>
+  <tr className="coin-row">
+    <td>{name}</td>
+    <td>{ticker}</td>
+    <td>{price} USD</td>
+  </tr>
 );
+
+Coin.propTypes = {
+  name: PropTypes.string.isRequired,
+  ticker: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
 
 export default Coin;
