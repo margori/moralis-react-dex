21a1a065555099d6db8b42f51b3ca6ea7efa1ca0
Marcelo Briones <ing@marcelobriones.com.ar>
2022-03-23 02:58:33 -0300

    Random changes of price of coins

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Coin.css';
 import PropTypes from 'prop-types';
 
-const Coin = ({ name, ticker, price }) => (
-  <tr className="coin-row">
-    <td>{name}</td>
-    <td>{ticker}</td>
-    <td>{price} USD</td>
-  </tr>
-);
+const Coin = ({ name, ticker, price }) => {
+  const [currentPrice, setCurrentPrice] = useState(price);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentPrice(1);
+      const randomPercentage = 0.995 + Math.random() * 0.01;
+      const newPrice = currentPrice * randomPercentage;
+      setCurrentPrice(newPrice);
+    }, 1000);
+    return () => clearInterval(timer);
+  });
+
+  return (
+    <tr className="coin-row">
+      <td>{name}</td>
+      <td>{ticker}</td>
+      <td>{Math.round(currentPrice * 100) / 100} USD</td>
+    </tr>
+  );
+};
 
 Coin.propTypes = {
   name: PropTypes.string.isRequired,
